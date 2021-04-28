f7cf167e391fa1785cd74bfc94a22a3d9d396be8
Mihai Borz <46225133+mibo32@users.noreply.github.com>
2021-04-28 08:04:29 +0200

    Update deploy-team.sh

--- terraform/deploy-team.sh
+++ terraform/deploy-team.sh
@@ -76,7 +76,7 @@ echo "initializing terraform state storage..."
 terraform init -backend-config="storage_account_name=$TERRAFORM_STORAGE_NAME" -backend-config="container_name=tfstate" -backend-config="access_key=$TERRAFORM_STORAGE_KEY" -backend-config="key=codelab.microsoft.tfstate" ./team
 
 echo "planning terraform..."
-terraform plan -out out.plan -var="deployment_name=$team_name" -var="location=$location" -var="tenant_id=$tenantid" -var="subscription_id=$subscriptionid" -chdir="./team" 
+terraform plan -out out.plan -var="deployment_name=$team_name" -var="location=$location" -var="tenant_id=$tenantid" -var="subscription_id=$subscriptionid" ./team
 
 echo "running terraform apply..."
 terraform apply out.plan
